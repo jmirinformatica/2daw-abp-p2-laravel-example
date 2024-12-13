2d0f30cdf99cfd16a231b3ecd0b8869ef7e40d1d
Pep <jjimen2@xtec.cat>
2024-12-13 18:03:45 +0100

    TW Elements components

--- bloggy/resources/js/app.js
+++ bloggy/resources/js/app.js
@@ -5,3 +5,11 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+/** 
+ * TW Elements
+ * @see https://tw-elements.com/docs/standard/integrations/laravel-integration/
+ */ 
+import { Dropdown, Ripple, initTWE } from "tw-elements";
+
+initTWE({ Dropdown, Ripple });
--- bloggy/resources/views/welcome.blade.php
+++ bloggy/resources/views/welcome.blade.php
@@ -55,12 +55,44 @@
                                     @endif
                                 @endauth
                                 @endif
-                                <a
-                                    href="{{ route('contact.form') }}"
-                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                >
-                                    Contact
-                                </a>
+                                <div class="relative" data-twe-dropdown-ref>
+                                    <a
+                                        class="flex items-center rounded bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                                        href="#"
+                                        type="button"
+                                        id="dropdownMenuButton2"
+                                        data-twe-dropdown-toggle-ref
+                                        aria-expanded="false"
+                                        data-twe-ripple-init
+                                        data-twe-ripple-color="light">
+                                        Menu
+                                        <span class="ms-2 w-2 [&>svg]:h-5 [&>svg]:w-5">
+                                            <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor">
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                                clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+                                    </a>
+                                    <ul
+                                        class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-base shadow-lg data-[twe-dropdown-show]:block dark:bg-surface-dark"
+                                        aria-labelledby="dropdownMenuButton1"
+                                        data-twe-dropdown-menu-ref>
+                                        <li>
+                                            <a
+                                                class="block w-full whitespace-nowrap bg-white px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-zinc-200/60 focus:bg-zinc-200/60 focus:outline-none active:bg-zinc-200/60 active:no-underline dark:bg-surface-dark dark:text-white dark:hover:bg-neutral-800/25 dark:focus:bg-neutral-800/25 dark:active:bg-neutral-800/25"
+                                                href="{{ route('contact.form') }}"
+                                                data-twe-dropdown-item-ref
+                                            >
+                                                Contact
+                                            </a>                                            
+                                        </li>
+                                    </ul>
+                                </div>
                             </nav>
                     </header>
 
