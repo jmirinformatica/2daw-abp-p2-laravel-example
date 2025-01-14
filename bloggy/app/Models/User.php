f00559ad69575c45a6abc20fe434c9788bbe1011
Pep <jjimen2@xtec.cat>
2025-01-14 15:58:41 +0100

    FIX Avatar URL attr

--- bloggy/app/Models/User.php
+++ bloggy/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Support\Facades\Storage;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasAvatar;
